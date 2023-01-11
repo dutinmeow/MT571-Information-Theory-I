5995338d730487d4087a33d317f5da9a45d83079
Dustin Miao <dutin20.meow@gmail.com>
2023-01-11 01:13:35 -0800

    .

--- battleship/m.cpp
+++ battleship/m.cpp
@@ -2,6 +2,28 @@
 #include <local/random>
 #include <local/debug>
 
+namespace std {
+
+template<class Fun>
+class y_combinator_result {
+	Fun fun_;
+public:
+	template<class T>
+	explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
+
+	template<class ...Args>
+	decltype(auto) operator()(Args &&...args) {
+		return fun_(std::ref(*this), std::forward<Args>(args)...);
+	}
+};
+
+template<class Fun>
+decltype(auto) y_combinator(Fun &&fun) {
+	return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+} // namespace std
+
 const bool FULL_AUTO = true;      // allows program to test itself
 const bool CONFIRM_VALID = false; // user confirmation that input data is correct
 
@@ -499,9 +521,23 @@ int main() {
 
 			std::cerr << YELLOW << "--> Step 0.2: Generate ship masks\n" << RESET;
 
-			for (int i = 0; i < numShips; i++) {
-				std::cerr << PURPLE << "ship i has " << shipPositions[i].size() << " possible positions\n";
+			std::array<std::array<short, N>, N> visBoard; 
+			std::array<std::array<short, N>, N> mask;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					visBoard[i][j] = UNKNOWN_INT;
+					mask[i][j] = 0;
+				}
 			}
+
+			std::y_combinator([&](auto self, int i) -> void {
+				if (i == numShips) {
+					return;
+				}
+
+				short len = shipSizes[i];
+				self(i + 1);
+			})(0);
 		}
 	}
 
